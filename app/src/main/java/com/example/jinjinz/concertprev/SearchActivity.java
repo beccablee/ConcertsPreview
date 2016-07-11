78927022250c9d41013e38c0c6f6ec75aa1f883e
Nneoma Oradiegwu <noradiegwu@fb.com>
2016-07-11 15:26:00 -0700

    commit

--- app/src/main/java/com/example/jinjinz/concertprev/SearchActivity.java
+++ app/src/main/java/com/example/jinjinz/concertprev/SearchActivity.java
@@ -1,26 +1,80 @@
 package com.example.jinjinz.concertprev;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.jinjinz.concertprev.fragments.ConcertsFragment;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements LocationListener {
     ConcertsFragment cFragment;
+    protected Context context;
+    TextView tvLocationTest;
     Button player;
     Button concert;
+
+    // Location variables
+    protected String latitude, longitude;
+    protected String latlong;
+    protected String permissions; // array or nah
+    protected boolean gps_enabled, network_enabled;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    private int LOCATION_PERMISSIONS = 10;
+    private String[] locationPermissions = {Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
+    // private GoogleApiClient mGoogleApiClient;
+    // private LocationRequest mLocationRequest;*/
+    // private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    // private long FASTEST_INTERVAL = 2000; /* 2 sec */
+    // Google Location API: AIzaSyBHPJnNxwfY8H_Uo6eGsbKw7Xp8Yag3xUM
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        // Find the toolbar view inside the activity layout
+        Toolbar tbSearch = (Toolbar) findViewById(R.id.tbSearch);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(tbSearch);
+
+        // Location setup
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            // Request location permissions
+            ActivityCompat.requestPermissions(SearchActivity.this, locationPermissions, LOCATION_PERMISSIONS);
+
+            return;
+        }
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+
+
         player = (Button) findViewById(R.id.playerBtn);
         concert = (Button) findViewById(R.id.concertBtn);
+        tvLocationTest = (TextView) findViewById(R.id.tvLocationTest);
+
 
         player.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +99,37 @@ public class SearchActivity extends AppCompatActivity {
             ft.commit();
         }
 
+    }
 
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = String.valueOf(location.getLatitude());
+        longitude = String.valueOf(location.getLongitude());
+        latlong = latitude + "," + longitude;
+        tvLocationTest.setText(latlong);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
     }
 }
+
