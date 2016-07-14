eca4d0773e75af68cf198e1ea24d2771de8b65fa
Nneoma Oradiegwu <noradiegwu@fb.com>
2016-07-14 01:24:32 -0700

    still working on location, it's the worst

--- app/src/main/java/com/example/jinjinz/concertprev/SearchActivity.java
+++ app/src/main/java/com/example/jinjinz/concertprev/SearchActivity.java
@@ -1,11 +1,15 @@
 package com.example.jinjinz.concertprev;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,7 +22,6 @@ import com.example.jinjinz.concertprev.fragments.ConcertsFragment;
 import com.example.jinjinz.concertprev.models.Concert;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -28,15 +31,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcels;
 
+import java.text.MessageFormat;
+
 import cz.msebera.android.httpclient.Header;
 
 
 //@RuntimePermissions
-public class SearchActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ConcertsFragment.ConcertsFragmentListener {
-    /*implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        com.google.android.gms.location.LocationListener*/
+public class SearchActivity extends AppCompatActivity implements com.google.android.gms.location.LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ConcertsFragment.ConcertsFragmentListener {
+
     ConcertsFragment cFragment;
     protected Context context;
     TextView tvLocationTest;
@@ -47,18 +49,21 @@ public class SearchActivity extends AppCompatActivity implements GoogleApiClient
     // client call variables
     Concert event = new Concert();
 
+    /////////////////////////////////////////////
+    private Location mLastLocation;
+    public LocationManager mLocationManager;
 
+    int updates;
+    ////////////////////////////////////////////////
     // Location variables
     protected String latitude, longitude;
     protected String latlong;
     protected String permissions; // array or nah
-    private int LOCATION_PERMISSIONS = 10;
+    private static final int LOCATION_PERMISSIONS = 10;
     private String[] locationPermissions = {Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
-    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
-    private long FASTEST_INTERVAL = 2000; /* 2 sec */
+    private LocationManager locationManager;
+    private String provider;
     // Google Location API: AIzaSyBHPJnNxwfY8H_Uo6eGsbKw7Xp8Yag3xUM
 
     @Override
@@ -66,18 +71,8 @@ public class SearchActivity extends AppCompatActivity implements GoogleApiClient
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-/*        // Create the location client to start receiving updates
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API).addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this).build();*/
-
-/*
-        // Find the toolbar view inside the activity layout
-        Toolbar tbSearch = (Toolbar) findViewById(R.id.tbSearch);
-        // Sets the Toolbar to act as the ActionBar for this Activity window.
-        // Make sure the toolbar exists in the activity and is not null
-        setSupportActionBar(tbSearch);
-*/
+        updates = 0;
+        handlePermissionsAndGetLocation();
 
         /*responseText = (TextView) findViewById(R.id.response);
 
@@ -141,19 +136,81 @@ public class SearchActivity extends AppCompatActivity implements GoogleApiClient
 
     }
 
-/*    // Menu icons are inflated just as they were with actionbar
+    ////////////////////////////////////////
     @Override
-    //        @NeedsPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
-    }*/
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Accepted
+                    getLocation();
+                } else {
+                    // Denied
+                    Toast.makeText(SearchActivity.this, "LOCATION Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 
+    @TargetApi(Build.VERSION_CODES.M)
+    private void handlePermissionsAndGetLocation() {
+        Log.d("tryingagain", "handlePermissionsAndGetLocation");
+        int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSIONS);
+            return;
+        }
+        getLocation();//if already has permission
+    }
 
-    // Google locationservices API
+    @TargetApi(Build.VERSION_CODES.M)
+    protected void getLocation() {
+        Log.d("tryingagain", "GetLocation");
+        int LOCATION_REFRESH_TIME = 1000;
+        int LOCATION_REFRESH_DISTANCE = 5;
+
+        if (!(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            Log.v("WEAVER_", "Has permission");
+            mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                    LOCATION_REFRESH_DISTANCE, mLocationListener);
+        } else {
+            Log.v("WEAVER_", "Does not have permission");
+        }
+
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.v("WEAVER_", "Location Change");
+            tvLocationTest.setText(String.valueOf(location.getLatitude()));
+            latlong = (MessageFormat.format("{0},{1}", location.getLatitude(), location.getLongitude()));
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    public void onConnected(Bundle bundle) {
 
     }
 
@@ -163,19 +220,23 @@ public class SearchActivity extends AppCompatActivity implements GoogleApiClient
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
+    ///////////////////////////////////////////
 
     AsyncHttpClient client;
 
     @Override
-    public void populateConcertsNearYou(final ConcertsFragment fragment) {
+    public void populateConcerts(final ConcertsFragment fragment, String query) {
         // url: includes api key and music classification
         String eventsURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&classificationName=Music";
         // the parameter(s)
         RequestParams params = new RequestParams();
         //params.put("latlong", dummyLatlong);
+        if (query != null) {
+            params.put("keyword", query); // get from search toolbar in ConcertsFragment
+        }
         params.put("latlong", "29.563034,-95.262090"); // must be N, E (in the us the last should def be -) that num + is W
         params.put("radius", "50");
         params.put("size", "200");
@@ -212,6 +273,11 @@ public class SearchActivity extends AppCompatActivity implements GoogleApiClient
         intent.putExtra("concert", Parcels.wrap(concert));
         Toast.makeText(this, concert.getEventName(), Toast.LENGTH_SHORT).show();
         startActivity(intent);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        tvLocationTest.setText("Latitude: " + location.getLatitude());
     }
 
 
